ab0f0ccf885b2d538f6ee63fa4030ef994c92a85
hageldave <david@RECHENKNECHTLNX>
2013-04-05 21:08:19 +0200

    Enum for Renaming shortcuts

---
+++ MetaDataEditor/src/util/RenameKey.java
@@ -0,0 +1,10 @@
+package util;
+
+/**
+ * Enum containing values used as shortcuts to rename files
+ * according to the FieldKey mapped to the RenameKey
+ * @author David Haegele
+ */
+public enum RenameKey {
+	$a,$b,$c,$d,$e,$f,$g,$h,$i,$j,$k,$l,$m,$n,$o,$p,$q,$r,$s,$t,$u,$v,$w,$x,$y,$z
+}
