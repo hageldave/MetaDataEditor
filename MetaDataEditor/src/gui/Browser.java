0c24ee4726fdf1d983998fa106014dd4b4250dc3
hageldave <david@RECHENKNECHTLNX>
2013-04-06 14:27:21 +0200

    bit of refactoring

--- MetaDataEditor/src/gui/Browser.java
+++ MetaDataEditor/src/gui/Browser.java
@@ -7,6 +7,7 @@ import gui.util.FileTreePanel.FileTreeNode;
 
 import java.awt.Component;
 import java.awt.event.ActionEvent;
+import java.io.File;
 
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
@@ -25,8 +26,9 @@ public class Browser extends RelativeLayoutPanel {
 	
 	private static final long serialVersionUID = 1L;
 	
-	/** JList die Musikdateien anzeigt */
-	FileList musicFileList;
+	/** Platzhalter label fuer das Laden des FileViews */
+	private static final JLabel loadingLabel = new JLabel("loading Filesystem...", JLabel.CENTER);
+	
 	
 	public Browser() {
 		loadFileView();
@@ -54,52 +56,12 @@ public class Browser extends RelativeLayoutPanel {
 	 * da dies evtl viel Zeit in anspruch nimmt.
 	 */
 	private void loadFileView() {
-		// loading label als platzhalter
-		final JLabel loadingLabel = new JLabel("loading Filesystem...", JLabel.CENTER);
+		// loading label als platzhalter hinzufuegen
 		this.add(loadingLabel, 0.5f,0.5f,true,0.8f,0.8f);
 		buildRefreshBttn();
 		
 		// loader thread
-		Thread loader = new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				// components erstellen
-				musicFileList = new FileList();
-				final FileTreePanel folderPane = new FileTreePanel();
-				final JScrollPane filePane = new JScrollPane(musicFileList);
-				// scrollbars der panes anzeigen
-				folderPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-				filePane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-				// listener des trees
-				folderPane.setTreeSelectionListener(new TreeSelectionListener() {
-					
-					@Override
-					// listet dateien des gewaehlten ordners in der musicFileList auf
-					public void valueChanged(TreeSelectionEvent e) {
-						FileTreeNode node = (FileTreeNode)
-								folderPane.getTree().getLastSelectedPathComponent();
-						if(node != null && !node.isTreeRoot()){
-							System.out.println(node.getFile());
-							if(node.getFile().exists()){
-								musicFileList.setListData(node.getFile().listFiles());
-							} else {
-								// wenn ausgewaehlter ordner nichtmehr existiert
-								folderPane.reloadTree();
-							}
-						}
-						
-					}
-				});
-				// platzhalter label ersetzen
-				remove(loadingLabel);
-				add(folderPane, 0f, 0.1f, false, 0.5f, 0.9f);
-				add(filePane, 0.5f, 0f, false, 0.5f, 1);
-				// frame erneuern durch kurzes verstecken
-				Overview.getMainFrame().setVisible(false);
-				Overview.getMainFrame().setVisible(true);
-			}
-		});
+		Thread loader = new Thread(new FileviewLoadingRoutine());
 		// loader thread starten
 		loader.start();
 	}
@@ -112,15 +74,62 @@ public class Browser extends RelativeLayoutPanel {
 		loadFileView();
 	}
 	
-	/** test */
-	public static void main(String[] args) {
-		JFrame myframe = new JFrame();
-		myframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myframe.add(new Browser());
-		myframe.setSize(500, 400);
-		myframe.setVisible(true);
-		Overview.setMainFrame(myframe);
+	
+	private class FileviewLoadingRoutine implements Runnable {
+		@Override
+		public void run() {
+			// components erstellen
+			final FileList musicFileList = new FileList();
+			final FileTreePanel folderPane = new FileTreePanel();
+			final JScrollPane filePane = new JScrollPane(musicFileList);
+			
+			// scrollbars der panes anzeigen
+			folderPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+			filePane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+			
+			// listener des trees
+			folderPane.setTreeSelectionListener(new TreeSelectionListener() {
+				@Override
+				// listet dateien des gewaehlten ordners in der musicFileList auf
+				public void valueChanged(TreeSelectionEvent e) {
+					FileTreeNode node = (FileTreeNode)
+							folderPane.getTree().getLastSelectedPathComponent();
+					if(node != null && !node.isTreeRoot()){
+						System.out.println(node.getFile());
+						if(node.getFile().exists()){
+							File folder = node.getFile();
+							File[] audioFiles = folder.listFiles(new org.jaudiotagger.audio.AudioFileFilter(false));
+							musicFileList.setListData(audioFiles);
+						} else {
+							// wenn ausgewaehlter ordner nichtmehr existiert
+							folderPane.reloadTree();
+						}
+					}
+				}
+			});
+			
+			// TODO: Listselectionlistener für musicFileList erstellen
+			
+			// platzhalter label ersetzen
+			remove(loadingLabel);
+			add(folderPane, 0f, 0.1f, false, 0.5f, 0.9f);
+			add(filePane, 0.5f, 0f, false, 0.5f, 1);
+			
+			// frame erneuern durch kurzes verstecken
+			Overview.getMainFrame().setVisible(false);
+			Overview.getMainFrame().setVisible(true);
+		}
 	}
+	
+//	/** test */
+//	public static void main(String[] args) {
+//		JFrame myframe = new JFrame();
+//		myframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		myframe.add(new Browser());
+//		myframe.setSize(500, 400);
+//		myframe.setVisible(true);
+//		Overview.setMainFrame(myframe);
+//	}
 	
 	
 }
--- MetaDataEditor/src/gui/CheckBoxPanel.java
+++ MetaDataEditor/src/gui/CheckBoxPanel.java
@@ -20,13 +20,14 @@ import main.Overview;
 
 @SuppressWarnings("serial")
 public class CheckBoxPanel extends RelativeLayoutPanel {
-
+	
 	MetaTypeCheckBox titel = new MetaTypeCheckBox("Titel", FieldKey.TITLE);
 	MetaTypeCheckBox interpret = new MetaTypeCheckBox("Interpret", FieldKey.ARTIST);
 	MetaTypeCheckBox album = new MetaTypeCheckBox("Album", FieldKey.ALBUM);
 	MetaTypeCheckBox jahr = new MetaTypeCheckBox("Jahr", FieldKey.YEAR);
 	MetaTypeCheckBox titelnummer = new MetaTypeCheckBox("Titelnummer", FieldKey.TRACK);
 	MetaTypeCheckBox genre = new MetaTypeCheckBox("Genre", FieldKey.GENRE);
+	// TODO: noch mehr Checkboxes
 	
 	private MetaTypeCheckBox[] allCheckBoxes;
 	private Component[] listData;
@@ -80,12 +81,12 @@ public class CheckBoxPanel extends RelativeLayoutPanel {
 	}
 	
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		frame.add(new CheckBoxPanel());
-		frame.setSize(400, 400);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+//	public static void main(String[] args) {
+//		JFrame frame = new JFrame();
+//		frame.add(new CheckBoxPanel());
+//		frame.setSize(400, 400);
+//		frame.setVisible(true);
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//	}
 
 }
